d1f0baaa955fe49473417e18b14191b2929a82a9
Enoch208 <ifeoluwaidowu2008@gmail.com>
2024-12-13 17:11:11 +0100

    added default state

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -5,7 +5,7 @@ const ThemeContext = createContext();
 export const ThemeProvider = ({ children }) => {
   const [darkMode, setDarkMode] = useState(() => {
     const savedTheme = localStorage.getItem('theme');
-    return savedTheme ? JSON.parse(savedTheme) : false;
+    return savedTheme ? JSON.parse(savedTheme) : true;
   });
 
   useEffect(() => {
